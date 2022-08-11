2e42b23a7f9d761069e30da24a9d834ae87f25a3
enzok <7831008+enzok@users.noreply.github.com>
2022-08-11 13:46:35 +0000

    update

--- static/js/playback.js
+++ static/js/playback.js
@@ -19,214 +19,212 @@
 
 function initExamplePlayer(playback_url) {
     "use strict";
-
+    
     /**
      * The URL of the Guacamole session recording which should be played back.
      *
      * @constant
      * @type String
      */
-    var terminal_url = 'https://' +  location.hostname + "/guac/playback/recfile/" + playback_url;
-    var RECORDING_URL = terminal_url;
 
-    var init = function() {
-        /**
-         * The element representing the session recording player.
-         *
-         * @type Element
-         */
-        var player = document.getElementById('player');
+     var terminal_url = 'https://' +  location.hostname + "/guac/playback/recfile/" + playback_url;
+     var RECORDING_URL = terminal_url;
 
-        /**
-         * The element which will contain the recording display.
-         *
-         * @type Element
-         */
-        var display = document.getElementById('display');
+    /**
+     * The element representing the session recording player.
+     *
+     * @type Element
+     */
+    var player = document.getElementById('player');
 
-        /**
-         * Play/pause toggle button.
-         *
-         * @type Element
-         */
-        var playPause = document.getElementById('play-pause');
+    /**
+     * The element which will contain the recording display.
+     *
+     * @type Element
+     */
+    var display = document.getElementById('display');
 
-        /**
-         * Button for cancelling in-progress seek operations.
-         *
-         * @type Element
-         */
-        var cancelSeek = document.getElementById('cancel-seek');
+    /**
+     * Play/pause toggle button.
+     *
+     * @type Element
+     */
+    var playPause = document.getElementById('play-pause');
 
-        /**
-         * Text status display indicating the current playback position within the
-         * recording.
-         *
-         * @type Element
-         */
-        var position = document.getElementById('position');
+    /**
+     * Button for cancelling in-progress seek operations.
+     *
+     * @type Element
+     */
+    var cancelSeek = document.getElementById('cancel-seek');
 
-        /**
-         * Slider indicating the current playback position within the recording,
-         * and allowing the user to change the playback position.
-         *
-         * @type Element
-         */
-        var positionSlider = document.getElementById('position-slider');
+    /**
+     * Text status display indicating the current playback position within the
+     * recording.
+     *
+     * @type Element
+     */
+    var position = document.getElementById('position');
 
-        /**
-         * Text status display indicating the current length of the recording.
-         *
-         * @type Element
-         */
-        var duration = document.getElementById('duration');
+    /**
+     * Slider indicating the current playback position within the recording,
+     * and allowing the user to change the playback position.
+     *
+     * @type Element
+     */
+    var positionSlider = document.getElementById('position-slider');
 
-        /**
-         * The tunnel which should be used to download the Guacamole session
-         * recording.
-         *
-         * @type Guacamole.Tunnel
-         */
-        var tunnel = new Guacamole.StaticHTTPTunnel(RECORDING_URL);
+    /**
+     * Text status display indicating the current length of the recording.
+     *
+     * @type Element
+     */
+    var duration = document.getElementById('duration');
 
-        /**
-         * Guacamole.SessionRecording instance to be used to playback the session
-         * recording.
-         *
-         * @type Guacamole.SessionRecording
-         */
-        var recording = new Guacamole.SessionRecording(tunnel);
+    /**
+     * The tunnel which should be used to download the Guacamole session
+     * recording.
+     *
+     * @type Guacamole.Tunnel
+     */
+    var tunnel = new Guacamole.StaticHTTPTunnel(RECORDING_URL);
 
-        /**
-         * The Guacamole.Display which displays the recording during playback.
-         *
-         * @type Guacamole.Display
-         */
-        var recordingDisplay = recording.getDisplay();
+    /**
+     * Guacamole.SessionRecording instance to be used to playback the session
+     * recording.
+     *
+     * @type Guacamole.SessionRecording
+     */
+    var recording = new Guacamole.SessionRecording(tunnel);
 
-        /**
-         * Converts the given number to a string, adding leading zeroes as necessary
-         * to reach a specific minimum length.
-         *
-         * @param {Numer} num
-         *     The number to convert to a string.
-         *
-         * @param {Number} minLength
-         *     The minimum length of the resulting string, in characters.
-         *
-         * @returns {String}
-         *     A string representation of the given number, with leading zeroes
-         *     added as necessary to reach the specified minimum length.
-         */
-        var zeroPad = function zeroPad(num, minLength) {
+    /**
+     * The Guacamole.Display which displays the recording during playback.
+     *
+     * @type Guacamole.Display
+     */
+    var recordingDisplay = recording.getDisplay();
 
-            // Convert provided number to string
-            var str = num.toString();
+    /**
+     * Converts the given number to a string, adding leading zeroes as necessary
+     * to reach a specific minimum length.
+     *
+     * @param {Numer} num
+     *     The number to convert to a string.
+     *
+     * @param {Number} minLength
+     *     The minimum length of the resulting string, in characters.
+     *
+     * @returns {String}
+     *     A string representation of the given number, with leading zeroes
+     *     added as necessary to reach the specified minimum length.
+     */
+    var zeroPad = function zeroPad(num, minLength) {
 
-            // Add leading zeroes until string is long enough
-            while (str.length < minLength)
-                str = '0' + str;
+        // Convert provided number to string
+        var str = num.toString();
 
-            return str;
+        // Add leading zeroes until string is long enough
+        while (str.length < minLength)
+            str = '0' + str;
 
-        };
+        return str;
 
-        /**
-         * Converts the given millisecond timestamp into a human-readable string in
-         * MM:SS format.
-         *
-         * @param {Number} millis
-         *     An arbitrary timestamp, in milliseconds.
-         *
-         * @returns {String}
-         *     A human-readable string representation of the given timestamp, in
-         *     MM:SS format.
-         */
-        var formatTime = function formatTime(millis) {
-
-            // Calculate total number of whole seconds
-            var totalSeconds = Math.floor(millis / 1000);
-
-            // Split into seconds and minutes
-            var seconds = totalSeconds % 60;
-            var minutes = Math.floor(totalSeconds / 60);
-
-            // Format seconds and minutes as MM:SS
-            return zeroPad(minutes, 2) + ':' + zeroPad(seconds, 2);
-
-        };
-
-        // Add playback display to DOM
-        display.appendChild(recordingDisplay.getElement());
-
-        // Begin downloading the recording
-        recording.connect();
-
-        // If playing, the play/pause button should read "Pause"
-        recording.onplay = function() {
-            playPause.textContent = 'Pause';
-        };
-
-        // If paused, the play/pause button should read "Play"
-        recording.onpause = function() {
-            playPause.textContent = 'Play';
-        };
-
-        // Toggle play/pause when display or button are clicked
-        display.onclick = playPause.onclick = function() {
-            if (!recording.isPlaying())
-                recording.play();
-            else
-                recording.pause();
-        };
-
-        // Resume playback when cancel button is clicked
-        cancelSeek.onclick = function cancelSeekOperation(e) {
-            recording.play();
-            player.className = '';
-            e.stopPropagation();
-        };
-
-        // Fit display within containing div
-        recordingDisplay.onresize = function displayResized(width, height) {
-
-            // Do not scale if display has no width
-            if (!width)
-                return;
-
-            // Scale display to fit width of container
-            recordingDisplay.scale(display.offsetWidth / width);
-
-        };
-
-        // Update slider and status when playback position changes
-        recording.onseek = function positionChanged(millis) {
-            position.textContent = formatTime(millis);
-            positionSlider.value = millis;
-        };
-
-        // Update slider and status when duration changes
-        recording.onprogress = function durationChanged(millis) {
-            duration.textContent = formatTime(millis);
-            positionSlider.max = millis;
-        };
-
-        // Seek within recording if slider is moved
-        positionSlider.onchange = function sliderPositionChanged() {
-
-            // Seek is in progress
-            player.className = 'seeking';
-
-            // Request seek
-            recording.seek(positionSlider.value, function seekComplete() {
-
-                // Seek has completed
-                player.className = '';
-
-            });
-
-        };
     };
 
-    init();
+    /**
+     * Converts the given millisecond timestamp into a human-readable string in
+     * MM:SS format.
+     *
+     * @param {Number} millis
+     *     An arbitrary timestamp, in milliseconds.
+     *
+     * @returns {String}
+     *     A human-readable string representation of the given timestamp, in
+     *     MM:SS format.
+     */
+    var formatTime = function formatTime(millis) {
+
+        // Calculate total number of whole seconds
+        var totalSeconds = Math.floor(millis / 1000);
+
+        // Split into seconds and minutes
+        var seconds = totalSeconds % 60;
+        var minutes = Math.floor(totalSeconds / 60);
+
+        // Format seconds and minutes as MM:SS
+        return zeroPad(minutes, 2) + ':' + zeroPad(seconds, 2);
+
+    };
+
+    // Add playback display to DOM
+    display.appendChild(recordingDisplay.getElement());
+
+    // Begin downloading the recording
+    recording.connect();
+
+    // If playing, the play/pause button should read "Pause"
+    recording.onplay = function() {
+        playPause.textContent = 'Pause';
+    };
+
+    // If paused, the play/pause button should read "Play"
+    recording.onpause = function() {
+        playPause.textContent = 'Play';
+    };
+
+    // Toggle play/pause when display or button are clicked
+    display.onclick = playPause.onclick = function() {
+        if (!recording.isPlaying())
+            recording.play();
+        else
+            recording.pause();
+    };
+
+    // Resume playback when cancel button is clicked
+    cancelSeek.onclick = function cancelSeekOperation(e) {
+        recording.play();
+        player.className = '';
+        e.stopPropagation();
+    };
+
+    // Fit display within containing div
+    recordingDisplay.onresize = function displayResized(width, height) {
+
+        // Do not scale if display has no width
+        if (!width)
+            return;
+
+        // Scale display to fit width of container
+        recordingDisplay.scale(display.offsetWidth / width);
+
+    };
+
+    // Update slider and status when playback position changes
+    recording.onseek = function positionChanged(millis) {
+        position.textContent = formatTime(millis);
+        positionSlider.value = millis;
+    };
+
+    // Update slider and status when duration changes
+    recording.onprogress = function durationChanged(millis) {
+        duration.textContent = formatTime(millis);
+        positionSlider.max = millis;
+    };
+
+    // Seek within recording if slider is moved
+    positionSlider.onchange = function sliderPositionChanged() {
+
+        // Seek is in progress
+        player.className = 'seeking';
+
+        // Request seek
+        recording.seek(positionSlider.value, function seekComplete() {
+
+            // Seek has completed
+            player.className = '';
+
+        });
+
+    };
+
 }
